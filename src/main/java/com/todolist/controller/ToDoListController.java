de367ac88b36d94d818841108644ccd101c433b1
maxim.stepanov <maxim.stepanov@4finance.com>
2022-12-04 11:50:06 +0200

    homework_2022_12_04

--- src/main/java/com/todolist/controller/ToDoListController.java
+++ src/main/java/com/todolist/controller/ToDoListController.java
@@ -2,9 +2,12 @@ package com.todolist.controller;
 
 import com.todolist.ToDoItems;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping("todolist")
@@ -18,11 +21,14 @@ public class ToDoListController {
     }
 
     @GetMapping(value = "/add-new-item", params = { "item" })
-    public void getWelcomePage(@RequestParam String item) {
-        toDoItems.addToList(item);
-        toDoItems.getToDoItems().stream().forEach(System.out::println);
-        System.out.println("Item added to new list");
+    public String addNewItem(@RequestParam String item) {
+       //to do
+        return ""; //<- this you can delete, add this just to make this method return something
     }
 
-//    @PostMapping(value = )
+    @GetMapping(value = "/show-to-do-items")
+    public List<String> showToDoItems() {
+        //to do
+        return List.of(); //<- this you can delete, add this just to make this method return something
+    }
 }
